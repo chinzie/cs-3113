785073e50a93d425db0ed5d19a0593d499f81420
Colin Hinzie <hinzieoak73@gmail.com>
2021-03-16 01:15:07 +0000

    single byte complete

--- cs3113sp21-project0/project0.c
+++ cs3113sp21-project0/project0.c
@@ -7,7 +7,8 @@
 
 struct BYTE
 {
-	unsigned int c[100000];
+	unsigned int c;
+	
 	int count;
 };
 typedef struct BYTE BYTE;
@@ -17,8 +18,15 @@ int main()
 {
        	int n;
 	
-	unsigned char *buf = (char *) malloc ( sizeof(char));
-	
+	unsigned char *buf = (char *) malloc (1000000 *  sizeof(char));
+
+
+
+
+	//unsigned int *e = (int *)  malloc (1000);
+
+
+
 	int i = 0;
 	int g = 0;
 	//int c;
@@ -27,86 +35,12 @@ int main()
 	{
 		buf[i] = *buf;
 		i++;
-		/*
-		
-		if (*buf <= 0x7F)
-		{
-			//printf("ascii\n");
-			buf[i] = *buf;
-			i++;
-		}
-		else if (*buf <= 0x07FF)
-		{
-			//printf("two byte\n");
-			buf[i] = *buf;
-			//buf[i] = (((*buf >> 6) & 0x1F) | 0xC0);
-			//buf[i+1] = (((*buf >> 0) & 0x3F) | 0x80);
-			
-			i++;
-		}
-		*/
-
-		//buf[i] = *buf;
-		//printf("%c\n", buf[i]);
-		//printf("%c\n", *buf);
-		//i++;
-
-		/*
-		for (int j = 0; j < temp; j++ && i > 0 )
-                {
-			printf("i is %d\n", i);
-			printf("j is %d\n", j);
-                        printf("buf[i] is %d\n", buf[i]);
-                        printf("buf[j] is %d\n", buf[j]);
-
-                        if (buf[i] == buf[j])
-                        {
-                                printf("already exists\n");
-                                buf[i] = 0;
-                                i--;
-                        }
-
-		}
-		}
-		
-		//char c = n & 0xFF;
-		printf("%d\n", *buf);
-		//printf("%d\n", c);
-		if(*buf <= 0x7F)
-		{
-			printf("ascii\n");
-			buf[i] = *buf;
-			i++;
-		}
-		else if(*buf <= 0x07FF)
-		{
-			printf("2 byte\n");
-
-			buf[i] = *buf;
-			i++;
-
-			n = read(0, buf, 1);
-			buf[i] = *buf;
-			i++;
-		}
-		else if(*buf <= 0xFFFF)
-		{
-			printf("3 byte\n");
-			n = read(0, buf, 2);
-		}
-		else if(*buf <= 0x10FFFF)
-		{
-			printf("4 byte\n");
-			n = read(0, buf, 3);
-		}
-		printf("i is: %d\n", i);
-		*/
-	
 	}
 
 	BYTE b[i];
 	
-	//to print multibyte, seek flag, combine, then print
+
+	
 	
         const unsigned char sevenpos = 0b10000000;
         const unsigned char sixpos = 0b01000000;
@@ -118,7 +52,7 @@ int main()
 
 	unsigned int o;
 
-	
+	/*
 	for (int u = 0; u < i; u++)
 	{
 		unsigned char j = buf[u];
@@ -131,13 +65,7 @@ int main()
 		}
 	}
 	printf("%c\n", o);
-	
-
-	for (int u = 0; u < i; u++)
-	{
-		printf("%c\n", buf[u]);
-	}
-	
+	*/
 	
 
 	
@@ -151,25 +79,50 @@ int main()
 		//printf("starting\n");
 		//
 
-		if (c & sevenpos && c & sixpos)
+		if (c & sevenpos && c & sixpos)//if it is a two byte char
 		{
+			printf("two byte char\n");
 			unsigned char p = buf[q+1];
 			y = 256 * c + p;
-			b[k].c[k] = y;
-			yeet++;
-			b[k].count = yeet;
+			for (int d = 0; d < k; d++)
+			{
+				if (y == b[d].c)
+				{
+					printf("two bite already exists\n");
+					break;
+				}
+				else if (y != b[d].c && d == k - 1)
+				{
+					printf("end of array\n");
+					for (int t = 0; t < i; t++)
+					{
+						printf("yup\n");
+						if (c == buf[t] && p == buf[t+1])
+						{
+							printf("showsup\n");
+							yeet++;
+						}
+						b[k].c = y;
+						yeet++;
+						b[k].count = yeet;
+						yeet = 0;
+						k++;
+						q++;
+						q++;
+						break;
+					}
+				}
+			}
 			//make edits for each case two, three, four
 			//including count
 			//and for first case .
-
-			q++;
 		}
 		
 		
 		if (k < 1)
 		{
 			//printf("first\n");
-			b[k].c[k] = c;
+			b[k].c = c;
 			for (int p = 0; p < i; p++)
 			{
 				if (c == buf[p])
@@ -185,12 +138,12 @@ int main()
 		{
 			for (int d = 0; d < k; d++)
 			{
-			       	if (c == b[d].c[d])
+			       	if (c == b[d].c)
 				{
 					//printf("%c already exists\n", c);
 					break;
 				}
-				else if (c != b[d].c[d] && d == k - 1)
+				else if (c != b[d].c && d == k - 1)
 				{
 					//printf("%c, does not exist\n", c);
 					for (int p = 0; p < i; p++)
@@ -200,10 +153,10 @@ int main()
                                                 	yeet++;
 						}
 					}
-					b[k].c[k] = c;
-					b[k].count = yeet;
-					yeet = 0;
-                                	k++;
+					b[k].c = c;
+					b[k].count = yeet;//set count value
+					yeet = 0;//reset count value
+                                	k++;//increment
 					break;
                         	}
                 	}
@@ -217,8 +170,10 @@ int main()
 	printf("now printing the array: \n");
 	for (int a = 0; a < k; a++)
 	{
-		printf("%c-->%d\n", b[a].c[a], b[a].count);
+		printf("%d-->%d\n", b[a].c, b[a].count);
 	}
+
+	//free(b);
 
        	return 0;
 }
